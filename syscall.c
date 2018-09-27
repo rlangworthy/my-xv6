b39246c5fb8905b0403e3f6ff5cd54fe0e852231
Robert Langworthy <robertlangworthy@Roberts-MacBook-Pro.local>
2018-09-27 18:15:46 -0500

    changed countTraps, syscall, proc.h to include total field

--- syscall.c
+++ syscall.c
@@ -139,7 +139,7 @@ syscall(void)
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->traps[num-1]++;
-    currproc->traps[0]++;
+    curproc->traps[0]++;
     curproc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
