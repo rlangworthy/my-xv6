e60da4361666e912fb1168d30113d90a026136b9
Robert Langworthy <robertlangworthy@Roberts-MacBook-Pro.local>
2018-10-03 21:00:49 -0500

    changed project2test to take an arg

--- project2test.c
+++ project2test.c
@@ -48,8 +48,8 @@ int main(int argc, char *argv[]){
     }
     
     int i = 0;
-    int runs = (int)&argv[1];
-    printf("%d\n", runs);
+    int runs = (int)argv[1];
+    printf(1, "%d\n", runs);
     if(runs > 0 && runs < 100){
 	    for(i=0;i<runs;i++)
 	    	uptime();
