db392241f79b8962dcc9c0625fd0514999562995
Robert Langworthy <robertlangworthy@Roberts-MacBook-Pro.local>
2018-11-27 22:53:24 -0600

    removed extra files

--- project0.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-
-int main(int argc, char *argv[])
-{
-  printf(1, "Hello World! My name is Robert and my CWID is A20433762\n");
-  exit();
-}
